63af458ef3f176b6a17934faff6c94e6d6806489
- <-@->
2024-08-09 15:37:21 +0200

    Added Rotation and Scale to smooth movement transition & fixed typo

--- AUIT/Assets/AUIT/PropertyTransitions/SmoothMovementTransition.cs
+++ AUIT/Assets/AUIT/PropertyTransitions/SmoothMovementTransition.cs
@@ -8,29 +8,79 @@ namespace AUIT.PropertyTransitions
     public class SmoothMovementTransition : PropertyTransition
     {
         [SerializeField]
-        private float transitionSpeed = 1.0f;
+
+        private float movementSpeed = 1.0f;
+        private float rotationSpeed = 1.0f;
+        private float scalingSpeed = 1.0f;
 
         public override void Adapt(Layout layout)
         {
-            StartCoroutine(SmoothMovement(layout, transitionSpeed));
+            StartCoroutine(SmoothMovement(layout));
         }
 
-        private IEnumerator SmoothMovement(Layout layout, float adaptationSpeed)
+        private bool transitionNotDone(float start, float speed)
         {
-            float startime = Time.time;
+            return (Time.time - start) * speed < 1f;
+        }
+
+        private IEnumerator SmoothMovement(Layout layout)
+        {
+            float starttime = Time.time;
             Vector3 startPosition = transform.position;
             Vector3 endPosition = layout.Position;
- 
-            // AdaptationManager.IsAdapting = true;
-            while (startPosition != endPosition && (Time.time - startime) * adaptationSpeed < 1f)
+            Quaternion startRotation = transform.rotation;
+            Quaternion endRotation = layout.Rotation;
+            Vector3 startScale = transform.localScale;
+            Vector3 endScale = layout.Scale;
+            // find minimal speed of all speeds to speed up calculation in while loop
+            // sort speeds in ascending order
+            float[] speeds = { movementSpeed, rotationSpeed, scalingSpeed };
+            System.Array.Sort(speeds);
+            // get minimal speed that is not 0
+            float minSpeed = 0;
+            foreach (float speed in speeds)
             {
-                Vector3 result = Vector3.Lerp(startPosition, endPosition, (Time.time - startime) * adaptationSpeed);
-                if (!float.IsNaN(result.x) && !float.IsNaN(result.y) && !float.IsNaN(result.z))
-                    transform.position = result;
-
-                yield return null;
+                if (speed > 0)
+                {
+                    minSpeed = speed;
+                    break;
+                }
             }
-            // AdaptationManager.IsAdapting = false;
+
+            // just run the loop if there is a speed > 0 (otherwise no transition is needed)
+            if (minSpeed > 0)
+            {
+                // run loop while transition is not done
+                while (startPosition != endPosition && this.transitionNotDone(starttime, minSpeed))
+                {
+                    // if movementSpeed > 0 & transition not done, interpolate
+                    if (movementSpeed > 0 && this.transitionNotDone(starttime, movementSpeed))
+                    {
+                        Vector3 resultPosition = Vector3.Lerp(startPosition, endPosition, (Time.time - starttime) * this.movementSpeed);
+                        if (!float.IsNaN(resultPosition.x) && !float.IsNaN(resultPosition.y) && !float.IsNaN(resultPosition.z))
+                            transform.position = resultPosition;
+                    }
+                    
+                    // if rotationSpeed > 0 & transition not done, interpolate
+                    if (rotationSpeed > 0 && this.transitionNotDone(starttime, rotationSpeed))
+                    {
+                        Quaternion resultRotation = Quaternion.Lerp(startRotation, endRotation, (Time.time - starttime) * this.rotationSpeed);
+                        if (!float.IsNaN(resultRotation.x) && !float.IsNaN(resultRotation.y) && !float.IsNaN(resultRotation.z) && !float.IsNaN(resultRotation.w))
+                            transform.rotation = resultRotation;
+                    }
+
+                    // if scalingSpeed > 0 & transition not done, interpolate
+                    if (scalingSpeed > 0 && this.transitionNotDone(starttime, scalingSpeed))
+                    {
+                        Vector3 resultScale = Vector3.Lerp(startScale, endScale, (Time.time - starttime) * this.scalingSpeed);
+                        if (!float.IsNaN(resultScale.x) && !float.IsNaN(resultScale.y) && !float.IsNaN(resultScale.z))
+                            transform.localScale = resultScale;
+                    }
+
+                    yield return null;
+                }
+                // AdaptationManager.IsAdapting = false;
+            }
         }
     }
 }
