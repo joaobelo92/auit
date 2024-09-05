441c954fd436d2a5111ec232a130c688bd905bb9
- <-@->
2024-09-05 08:41:05 +0200

    fixxed a bunch of bugs, added documentation for code and inspector and implemented multiple faster cubic bezier algorithms (so far stupid interpolation is still selected though, since the Newton Algorithms have kinks to be worked out)

--- AUIT/Assets/AUIT/PropertyTransitions/CubicBezierTransition.cs
+++ AUIT/Assets/AUIT/PropertyTransitions/CubicBezierTransition.cs
@@ -9,8 +9,13 @@ namespace AUIT.PropertyTransitions
     [System.Serializable]
     public class CubicBezier
     {
+        [Tooltip("The first control point of the cubic bezier curve.\n" +
+                 "The x value should be between (including) 0 and 1.")]
         [SerializeField]
         private Vector2 point1;
+
+        [Tooltip("The second control point of the cubic bezier curve.\n" +
+                 "The x value should be between (including) 0 and 1.")]
         [SerializeField]
         private Vector2 point2;
 
@@ -33,6 +38,7 @@ namespace AUIT.PropertyTransitions
             return this.point1 == list[0] && this.point2 == list[1];
         }
 
+        // TODO: optimize more by rearranging the formula
         private float cubicBezierFromPointDimensions(float t, float p1, float p2)
         {
             // first part of formula will always be 0 & last part will always be t**3 (since P3 is (1,1))
@@ -41,13 +47,56 @@ namespace AUIT.PropertyTransitions
                     Mathf.Pow(t, 3);
         }
 
-        public float getBezierValue(float time, float totalTime)
+        // TODO: optimize more by rearranging the formula
+        private float calculateTangent(float t, float p1, float p2)
         {
-            // TODO: implement better (by rearranging the formula)
-            // TODO: maybe precompute or improve step algorithm at least
-            // !!! VERY INEFFICIENT !!!
-            // For now interpolate
-            // find the t value that is closest to the time
+            // calculate the tangent of the curve at the given t value (1st derivate)
+            return  3 * (p1 - (4 * p1 + 3 * t * p1) * t) +
+                    3 * t * (2 * p2 - 3 * t * p2) +
+                    3 * t * t;
+        }
+
+        private float newtonsMethodToFindT(float time, float totalTime) {
+            // use Newton's method to find the t value that gives the closest x value to the time
+            // https://en.wikipedia.org/wiki/Newton's_method
+            // the minimal accuracy for the calculated time value
+            float accuracy = 0.01f / totalTime;
+            // the initial t value, choose current time value as approximation
+            float approximatedT = time;
+            // the difference between the time and calculated x value (max 1)
+            float diffTimeX = 1.0f;
+            // to prevent infinite loops, set a counter
+            // WARNING: limits the accuracy of the result (for very high total times)
+            int counter = 0;
+            while (Mathf.Abs(diffTimeX) > accuracy && counter < 100)
+            {
+                counter++;
+                // calculate the tangent of the curve at the current t value
+                float tangent = Mathf.Abs(this.calculateTangent(approximatedT, this.point1.x, this.point2.x));
+                // if the tangent is 0, we have found the correct t value
+                if (tangent == 0)
+                {
+                    break;
+                }
+                // calculate the bezier value at the current t value
+                diffTimeX = this.cubicBezierFromPointDimensions(approximatedT, this.point1.x, this.point2.x) - time;
+                Debug.Log("diffTimeX: " + diffTimeX + " time: " + time + " t: " + approximatedT + " tangent: " + tangent);
+                // calculate the new t value based on the tangent
+                approximatedT -= diffTimeX / tangent;
+                // DEBUG
+                if (approximatedT > 1 || approximatedT < 0)
+                {
+                    Debug.Log("Newton's method failed, t value out of bounds: " + approximatedT);
+                    break;
+                }
+            }
+            Debug.Log("Newton's method iterations: " + counter + " with diffTimeX: " + diffTimeX + " and found t: " + approximatedT);
+            return approximatedT;
+        }
+
+        // TODO: replace with newtons method
+        private float stupidMethodToFindT(float time, float totalTime)
+        {
             float accuracyRange = 0.01f / totalTime;
             float t = 0.5f;
             float stepsize = 0.5f;
@@ -67,8 +116,54 @@ namespace AUIT.PropertyTransitions
                 currentX = this.cubicBezierFromPointDimensions(t, this.point1.x, this.point2.x);
                 stepsize /= 2;
             } while (Mathf.Abs(currentX - time) > accuracyRange);
-            
+            return t;
+        }
+
+        private float firefoxMethodToFindT(float time, float totalTime)
+        {
+            float A(float aA1, float aA2) { return 1.0f - 3.0f * aA2 + 3.0f * aA1; }
+            float B(float aA1, float aA2) { return 3.0f * aA2 - 6.0f * aA1; }
+            float C(float aA1)  { return 3.0f * aA1; }
+
+            // Returns x(t) given t, x1, and x2, or y(t) given t, y1, and y2.
+            float CalcBezier(float aT, float aA1, float aA2) {
+                return ((A(aA1, aA2)*aT + B(aA1, aA2))*aT + C(aA1))*aT;
+            }
+
+            // Returns dx/dt given t, x1, and x2, or dy/dt given t, y1, and y2.
+            float GetSlope(float aT, float aA1, float aA2) {
+                return 3.0f * A(aA1, aA2)*aT*aT + 2.0f * B(aA1, aA2) * aT + C(aA1);
+            }
+
+            float GetTForX(float aX) {
+                // Newton raphson iteration
+                var aGuessT = aX;
+                var currentX = 0.0f;
+                for (var i = 0; i < 4; ++i) {
+                    var currentSlope = GetSlope(aGuessT, this.point1.x, this.point2.x);
+                    if (currentSlope == 0.0) return aGuessT;
+                    currentX = CalcBezier(aGuessT, this.point1.x, this.point2.x) - aX;
+                    aGuessT -= currentX / currentSlope;
+                }
+                Debug.Log("currentX: " + currentX);
+                return aGuessT;
+            }
+
+            return GetTForX(time);
+        }
+
+        // TODO: maybe improve performance with precalculated lookup table
+        public float getBezierValue(float time, float totalTime)
+        {
+            // test for linear bezier to improve performance
+            if (this.point1.x == this.point1.y && this.point2.x == this.point2.y)
+            {
+                return time;
+            }
+            // if not linear, do normal bezier calculation
+            float t = this.stupidMethodToFindT(time, totalTime);   
             float y = this.cubicBezierFromPointDimensions(t, this.point1.y, this.point2.y);
+            Debug.Log("t: " + t + " y: " + y + " time: " + time + " point1: " + this.point1 + " point2: " + this.point2);
             return y;
         }
     }
@@ -87,16 +182,20 @@ namespace AUIT.PropertyTransitions
         CubicBezier
         };
 
-        // transition speed
+        // transition speed 
+        [Tooltip("The duration of the transition in seconds.\n" +
+                 "If set to 0, the transition will be instant.")]
         [SerializeField]
         public float duration = 1.0f;
 
         // set ease as default
+        [Tooltip("The transition mode to use (similar to e.g. CSS transition modes).")]
         [SerializeField]
         private modes transitionMode = modes.Ease;
         private modes oldTransitionMode;
         
         // CubicBezierTransition
+        [Tooltip("The Cubic Bezier defining the transition curve.")]
         [SerializeField]
         private CubicBezier transitionBezier;
         private Vector2[] oldTransitionBezier;
@@ -111,17 +210,17 @@ namespace AUIT.PropertyTransitions
         // callback to when some values might have changed
         public void OnValidate()
         {
-            // if the transition mode has changed, update the bezier object
-            if (this.transitionMode != this.oldTransitionMode)
-            {
-                this.transitionBezier = modeToBezier(this.transitionMode);
-                updateOldValues();
-            }
-
             // if the bezier object has changed, update the transition mode
             if (!this.transitionBezier.equalsList(this.oldTransitionBezier))
             {
                 this.transitionMode = bezierToMode(this.transitionBezier);
+                updateOldValues();
+            }
+
+            // if the transition mode has changed, update the bezier object
+            if (this.transitionMode != this.oldTransitionMode)
+            {
+                this.transitionBezier = modeToBezier(this.transitionMode);
                 updateOldValues();
             }
         }
@@ -207,8 +306,8 @@ namespace AUIT.PropertyTransitions
         // get a value from 0-1 that represents the transition progress
         public float timespanToTransitionValue(float timespan)
         {
-            // if duration is 0, transition instantly
-            if (this.duration == 0)
+            // if duration is <= 0, transition instantly
+            if (this.duration <= 0)
             {
                 return 1;
             }
@@ -219,7 +318,6 @@ namespace AUIT.PropertyTransitions
             timepoint = Mathf.Min(1, Mathf.Max(0, timepoint));
 
             // calculate the transition value based on the current Bezier curve
-            // to improve performance, we give accurace TODO: remove when algorithm is improved
             return this.transitionBezier.getBezierValue(timepoint, this.duration);
         }
     }
@@ -258,33 +356,49 @@ namespace AUIT.PropertyTransitions
             Quaternion endRotation = layout.Rotation;
             Vector3 startScale = transform.localScale;
             Vector3 endScale = layout.Scale;
+            bool posDone = false;
+            bool rotDone = false;
+            bool scaleDone = false;
             float timespan = 0;
 
             // run loop while transitions are not done
-            while (startPosition != endPosition || startRotation != endRotation || startScale != endScale)
+            while (!posDone || !rotDone || !scaleDone)
             {
                 // if movementSpeed > 0 & transition not done, interpolate
                 if (movementBezier.duration > 0 && timespan < movementBezier.duration)
                 {
-                    Vector3 resultPosition = Vector3.Lerp(startPosition, endPosition, movementBezier.timespanToTransitionValue(timespan));
+                    Vector3 resultPosition = Vector3.LerpUnclamped(startPosition, endPosition, movementBezier.timespanToTransitionValue(timespan));
+                    Debug.Log(resultPosition);
                     if (!float.IsNaN(resultPosition.x) && !float.IsNaN(resultPosition.y) && !float.IsNaN(resultPosition.z))
                         transform.position = resultPosition;
+                } else if (timespan >= movementBezier.duration && !posDone)
+                {
+                    transform.position = endPosition;
+                    posDone = true;
                 }
 
                 // if rotationSpeed > 0 & transition not done, interpolate
                 if (rotationBezier.duration > 0 && timespan < rotationBezier.duration)
                 {
-                    Quaternion resultRotation = Quaternion.Lerp(startRotation, endRotation, rotationBezier.timespanToTransitionValue(timespan));
+                    Quaternion resultRotation = Quaternion.LerpUnclamped(startRotation, endRotation, rotationBezier.timespanToTransitionValue(timespan));
                     if (!float.IsNaN(resultRotation.x) && !float.IsNaN(resultRotation.y) && !float.IsNaN(resultRotation.z) && !float.IsNaN(resultRotation.w))
                         transform.rotation = resultRotation;
+                } else if (timespan >= rotationBezier.duration && !rotDone)
+                {
+                    transform.rotation = endRotation;
+                    rotDone = true;
                 }
 
                 // if scalingSpeed > 0 & transition not done, interpolate
                 if (scalingBezier.duration > 0 && timespan < scalingBezier.duration)
                 {
-                    Vector3 resultScale = Vector3.Lerp(startScale, endScale, scalingBezier.timespanToTransitionValue(timespan));
+                    Vector3 resultScale = Vector3.LerpUnclamped(startScale, endScale, scalingBezier.timespanToTransitionValue(timespan));
                     if (!float.IsNaN(resultScale.x) && !float.IsNaN(resultScale.y) && !float.IsNaN(resultScale.z))
                         transform.localScale = resultScale;
+                } else if (timespan >= scalingBezier.duration && !scaleDone)
+                {
+                    transform.localScale = endScale;
+                    scaleDone = true;
                 }
 
                 // update timespan
