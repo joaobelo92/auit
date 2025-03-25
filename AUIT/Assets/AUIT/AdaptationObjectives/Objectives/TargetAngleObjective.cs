e5eb10ad8ce75f89ead5493a3ed9011c1e5e0376
Yi Fei <cyf4799@gmail.com>
2025-03-25 11:58:19 -0400

    implementing a target angle and distance objective.

---
+++ AUIT/Assets/AUIT/AdaptationObjectives/Objectives/TargetAngleObjective.cs
@@ -0,0 +1,91 @@
+﻿using AUIT.AdaptationObjectives.Definitions;
+using AUIT.AdaptationObjectives.Extras;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace AUIT.AdaptationObjectives
+{
+    public class TargetAngleObjective : LocalObjective
+    {
+        [SerializeField]
+        private ContextSource<Transform> userContextSource;
+
+        [SerializeField]
+        private float targetAngle = 0f;
+
+        [SerializeField]
+        private float angleInterval = 5f;
+
+        public override float CostFunction(Layout optimizationTarget, Layout initialLayout = null)
+        {
+            if (userContextSource == null)
+            {
+                Debug.LogError("FieldOfViewObjective.CostFunction(): User context source is not set.");
+            }
+
+            // Idea: get angle between gaze and object vectors on y and x axis
+            // Then we define intervals that are acceptable, e.g. comprising near/mid/far peripheral view
+            // Cost function increases the further is is from that interval
+            Transform contextSourceTransform = userContextSource.GetValue();
+            Vector3 target = contextSourceTransform.worldToLocalMatrix.MultiplyPoint3x4(optimizationTarget.Position);
+
+            float angle = Vector3.Angle(Vector3.forward, target);
+
+            // Inspired by https://en.wikipedia.org/wiki/Peripheral_vision
+            // float cost = Mathf.Max(Mathf.Abs(rotation - boundaryOrigin[index]), boundaryDifference[index]) - boundaryDifference[index];
+            float angleDiff = Mathf.Abs(angle - targetAngle); 
+
+            float cost = Mathf.Min(angleDiff / angleInterval, 1);
+            return cost;
+        }
+
+        public override Layout OptimizationRule(Layout optimizationTarget, Layout initialLayout)
+        {
+            if (userContextSource == null)
+            {
+                Debug.LogError("FieldOfViewObjective.OptimizationRule(): User context source is not set.");
+            }
+
+            Transform contextSourceTransform = userContextSource.GetValue();
+            // Would be efficient to cache rotation when cost function is computed
+            Vector3 target = contextSourceTransform.worldToLocalMatrix.MultiplyPoint3x4(optimizationTarget.Position);
+
+            Layout result = optimizationTarget.Clone();
+
+            if (Random.value < 0.5f)
+            {
+                Vector3 move = contextSourceTransform.localToWorldMatrix.MultiplyPoint3x4(new Vector3(0, 0, target.magnitude)) - optimizationTarget.Position;
+                result.Position = optimizationTarget.Position + move * HelperMath.SampleNormalDistribution(0.1f, 0.1f);
+            }
+            else // move some cm at random
+            {
+                result.Position = optimizationTarget.Position + Random.insideUnitSphere * HelperMath.SampleNormalDistribution(0.5f, 0.5f) * 0.01f;
+            }
+
+            return result;
+        }
+
+        public override Layout DirectRule(Layout optimizationTarget)
+        {
+            // Transform contextSourceTransform = userContextSource.GetValue();
+            // Would be efficient to cache rotation when cost function is computed
+            // Vector3 target = contextSourceTransform.worldToLocalMatrix.MultiplyPoint3x4(optimizationTarget.Position);
+            // Quaternion quaternion = Quaternion.FromToRotation(target, Vector3.forward);
+
+            Layout result = optimizationTarget.Clone();
+            // result.Position = contextSourceTransform.localToWorldMatrix.MultiplyPoint(quaternion * target);
+
+            return result;
+        }
+
+        private new void OnEnable()
+        {
+            base.OnEnable();
+
+            if (userContextSource == null)
+            {
+                userContextSource = GetUserPoseContextSource();
+            }
+        }
+    }
+}
---
+++ AUIT/Assets/AUIT/AdaptationObjectives/Objectives/TargetDistanceObjective.cs
@@ -0,0 +1,94 @@
+﻿using AUIT.AdaptationObjectives.Definitions;
+using AUIT.AdaptationObjectives.Extras;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace AUIT.AdaptationObjectives
+{
+    public class TargetDistanceObjective : LocalObjective
+    {
+        // In this case optimization target must be a transform
+        // Add validation in the future
+        [SerializeField]
+        private ContextSource<Transform> targetContextSource;
+
+        [SerializeField]
+        private float targetDistance = 0.3f;
+
+        [SerializeField]
+        private float distanceInterval = 0.1f;
+
+        public override float CostFunction(Layout optimizationTarget, Layout initialLayout = null)
+        {
+            if (targetContextSource == null)
+            {
+                Debug.LogError("DistanceIntervalObjective.CostFunction(): Target context source is not set.");
+            }
+
+            Vector3 targetPosition = targetContextSource.GetValue().position;
+            Vector3 currentPosition = optimizationTarget.Position;
+
+            Vector3 distanceVector = targetPosition - currentPosition;
+            distanceVector.y = 0;
+            float distanceXZ = Mathf.Abs(distanceVector.magnitude - targetDistance);
+            return Mathf.Min(distanceXZ / distanceInterval, 1);
+        }
+
+        public override Layout OptimizationRule(Layout optimizationTarget, Layout initialLayout)
+        {
+            if (targetContextSource == null)
+            {
+                Debug.LogError("DistanceIntervalObjective.OptimizationRule(): Target context source is not set.");
+            }
+
+            Vector3 targetPosition = targetContextSource.GetValue().position;
+            Vector3 currentPosition = optimizationTarget.Position;
+            Vector3 displacement = targetPosition - currentPosition;
+            displacement.y = 0;
+            float distance = displacement.magnitude - targetDistance;
+            Vector3 direction = Mathf.Sign(distance) * displacement.normalized;
+
+            Layout result = optimizationTarget.Clone();
+
+
+            // Two different strategies
+            if (Random.value > 0.5f)
+            {
+                Vector3 position = optimizationTarget.Position + direction * HelperMath.SampleNormalDistribution(0.1f, 0.1f);
+                position.y = currentPosition.y + (targetPosition.y - currentPosition.y) * HelperMath.SampleNormalDistribution(0.1f, 0.1f);
+                result.Position = position;
+            }
+            else // just move at random
+            {
+                float x = HelperMath.SampleNormalDistribution(0.5f, 0.5f) * 0.01f;
+                float y = HelperMath.SampleNormalDistribution(0.5f, 0.5f) * 0.01f;
+                float z = HelperMath.SampleNormalDistribution(0.5f, 0.5f) * 0.01f;
+                Vector3 position = optimizationTarget.Position + new Vector3(x, y, z);
+                result.Position = position;
+            }
+
+            // Debug.Log($"{distance}, {distanceVector}, {result.Position} {CostFunction(optimizationTarget)} {CostFunction(result)}");
+            return result;
+        }
+
+        public override Layout DirectRule(Layout optimizationTarget)
+        {
+
+            //Vector3 distanceVector = GetDistanceVector(optimizationTarget);
+            Layout result = optimizationTarget.Clone();
+            //result.Position = optimizationTarget.Position + distanceVector;
+            return result;
+        }
+
+        private new void OnEnable()
+        {
+            base.OnEnable();
+
+            if (targetContextSource == null)
+            {
+                targetContextSource = GetUserPoseContextSource();
+            }
+        }
+
+    }
+}
